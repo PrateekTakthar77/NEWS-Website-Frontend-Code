0783c6031743cf017825a4a49d19e0e3ef8f6948
Prateek Takthar <prateektakthar184b@gmail.com>
2023-10-27 16:45:08 +0530

    create category and subcategory

--- src/components/makingcharges/makingCharges.jsx
+++ src/components/makingcharges/makingCharges.jsx
@@ -1,91 +1,81 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import {
   Button,
   FormControl,
-  Select,
   Input,
   FormLabel,
+  Radio,
+  RadioGroup,
 } from "@chakra-ui/react";
 import axios from "axios";
 
-function AddSubcategoryForm() {
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [subcategory, setSubcategory] = useState("");
+function AddCategoryAndSubcategoryForm() {
+  const [category, setCategory] = useState("");
+  const [subcategories, setSubcategories] = useState([]);
+  const [status, setStatus] = useState("active"); // Default status
 
-  // Fetch existing categories from the server when the component mounts
-  useEffect(() => {
-    axios
-      .get("http://localhost:5009/api/article/category/get")
-      .then((response) => {
-        setCategories(response.data); // Assuming the server provides a list of categories
-      });
-  }, []);
-
-  // const handleAddSubcategory = async () => {
-  //   try {
-  //     // Send a POST request to add the subcategory to the selected category
-  //     await axios.post(`/add-subcategory/${selectedCategory}`, { subcategory });
-  //     // Handle success or show a notification to the user
-  //     // You can use state or a notification library to inform the user.
-  //   } catch (error) {
-  //     // Handle errors and show an error message to the user
-  //   }
-  // };
-
-  const handleAddSubcategory = async () => {
+  const handleAddCategoryAndSubcategory = async () => {
     try {
-      if (selectedCategory === "" || subcategory === "") {
-        // Add client-side validation to ensure both fields are filled
-        // You can show an error message to the user.
-        console.error("Both fields are required.");
+      console.log(category);
+      console.log(subcategories);
+      console.log(status);
+      if (category === "" || subcategories.length === 0) {
+        console.error(
+          "Both category and at least one subcategory are required."
+        );
         return;
       }
 
-      // Send a POST request to add the subcategory to the selected category
-      await axios.post(
-        `http://localhost:5009/api/article/category/add/${selectedCategory}`,
-        {
-          subcategory,
-        }
-      );
+      // Send a POST request to add the category and subcategories
+      await axios.post("http://localhost:5009/api/article/category/", {
+        category,
+        subcategories,
+        status, // Include the status in the request
+      });
 
-      // Clear the subcategory input field after successful submission
-      setSubcategory("");
+      // Clear the input fields after successful submission
+      setCategory("");
+      setSubcategories([]);
 
       // Optionally, show a success message or update the UI to reflect the change.
     } catch (error) {
-      // Handle errors and show an error message to the user
-      console.error("Error adding subcategory:", error);
+      console.error("Error adding category and subcategories:", error);
     }
   };
 
   return (
     <form>
       <FormControl id="category">
-        <FormLabel>Select Category</FormLabel>
-        <Select
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-        >
-          {categories.map((category) => (
-            <option key={category._id} value={category._id}>
-              {category.name}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl id="subcategory">
-        <FormLabel>Add Subcategory</FormLabel>
+        <FormLabel>Category</FormLabel>
         <Input
           type="text"
-          value={subcategory}
-          onChange={(e) => setSubcategory(e.target.value)}
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
         />
       </FormControl>
-      <Button onClick={handleAddSubcategory}>Add Subcategory</Button>
+
+      <FormControl id="subcategories">
+        <FormLabel>Subcategories</FormLabel>
+        <Input
+          type="text"
+          value={subcategories.join(",")}
+          onChange={(e) => setSubcategories(e.target.value.split(","))}
+        />
+      </FormControl>
+
+      <FormControl id="status">
+        <FormLabel>Status</FormLabel>
+        <RadioGroup value={status} onChange={setStatus}>
+          <Radio value="active">Active</Radio>
+          <Radio value="disable">Disable</Radio>
+        </RadioGroup>
+      </FormControl>
+
+      <Button onClick={handleAddCategoryAndSubcategory} marginTop={"20px"}>
+        Add Category and Subcategories
+      </Button>
     </form>
   );
 }
 
-export default AddSubcategoryForm;
+export default AddCategoryAndSubcategoryForm;
